b0619374ec3f4ca0edfd43a472322c1b5f771e50
amieecode <enwelumchiamaka@gmail.com>
2024-08-19 21:11:54 +0100

    Implemented users registration

--- todoproject/todoapp/views.py
+++ todoproject/todoapp/views.py
@@ -1,4 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 
 # Create your views here.
 
@@ -6,6 +9,22 @@ def home(request):
     return render(request, 'todoapp/todo.html', {})
 
 def register(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+
+        if len(password) < 8:
+            messages.error(request, 'Password must be at least 8 characters')
+            return redirect('register')
+
+        get_all_users_by_username = User.objects.filter(username=username)
+        if get_all_users_by_username:
+            messages.error(request, 'Error, username already exists.')
+            return redirect('register')
+
+        new_user = User.objects.create_user(username=username, email=email, password=password)
+        new_user.save()
     return render(request, 'todoapp/register.html', {})
 
 def loginpage(request):
